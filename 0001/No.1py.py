634a5d71f26c62ffa27509664a9d887b5cd16936
loilisxka <43001784+loilisxka@users.noreply.github.com>
2018-09-07 13:27:28 +0800

    Delete No.1py.py

--- 0001/No.1py.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import hmac
-
-message = b'Hello,world'
-with open('No.1_py.txt', 'w') as f:
-    for i in range(100, 300):
-        i = str(i)
-        h = hmac.new(i.encode(encoding='utf-8'), message, digestmod = 'MD5')
-        f.write('%s\n' % h.hexdigest())
